79ea09b29ff572b33247c8244232c591f18ce26e
pure diablo <huangkemin1985@gmail.com>
2014-11-13 01:31:08 +0800

    fix unit test

--- src/main/java/com/banyou/backend/repository/ProductDescDao.java
+++ src/main/java/com/banyou/backend/repository/ProductDescDao.java
@@ -10,9 +10,10 @@ import com.banyou.backend.entity.ProductDesc;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductDescDao extends PagingAndSortingRepository<ProductDesc, Long> {
 	@Modifying
-	@Query("delete ProductDesc pd where pd.product.id=?0")
-	public int deleteByProductId(Long id);
+	@Query("delete ProductDesc pd where pd.product.id=:id")
+	public int deleteByProductId(@Param("id")Long id);
 }
